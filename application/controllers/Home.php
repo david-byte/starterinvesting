c689b088e8a47bdc4b5a64942049a02a06d300f8
David Anderson Developer <davidanderson04@live.com>
2020-12-11 17:02:42 -0300

    criação do controller home para teste

---
+++ application/controllers/Home.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('Ação não permitida');
+
+class Home extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('home/index');
+    }
+}
