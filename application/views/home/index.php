5b497e95c75fd51290551d575dca223ee2908f71
David Anderson Developer <davidanderson04@live.com>
2020-12-13 07:28:31 -0300

    criação dos elementos acessoria e cursos

---
+++ application/views/home/index.php
@@ -0,0 +1,29 @@
+<!-- Services-->
+<section class="page-section" id="services">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Nossos Serviços</h2>
+            <h3 class="section-subheading text-muted">Fazemos seu dinheiro render até 5% ao mês!!</h3>
+        </div>
+        <div class="row text-center">
+            <div class="col-md-6">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-bezier-curve fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 class="my-3">Acessoria Financeira</h4>
+                <p class="text-muted">Seu dinheiro é aplicado na bolsa sem complicação, nossa equipe opera tendo resultados de até 5% ao mês. As operações são feitas no mercado de câmbio de moedas chamado Forex, o maior mercado financeiro.</p>
+                <a class="btn btn-sm btn-dark" href="#">Ver Mais</a>
+            </div>
+            <div class="col-md-6">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 class="my-3">Curso</h4>
+                <p class="text-muted">Oferecemos cursos ONLINE! para o mercado de OB com o menor preço no mercado. Mostrandos as estratégias utilizadas, gerenciamento da banca e gatihlos para boas operações</p>
+                <a class="btn btn-sm btn-dark" href="#">Ver Mais</a>
+            </div>
+        </div>
+    </div>
+</section>
