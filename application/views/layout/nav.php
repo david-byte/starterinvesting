02ffa88c500865a36c7e19080133935090fb1298
David Anderson Developer <davidanderson04@live.com>
2020-12-22 08:38:44 -0300

    criando a rota de login

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -14,7 +14,7 @@
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Sobre</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">Time</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contato</a></li>
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#client">Já sou cliente</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="<?php echo base_url('auth/login'); ?>">Já sou cliente</a></li>
             </ul>
         </div>
     </div>
